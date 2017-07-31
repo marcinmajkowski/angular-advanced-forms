fbe587924d071d189bc417690cded02ab601fcab
Marcin Majkowski <pl.marcin.m@gmail.com>
2017-07-31 19:51:40 +0200

    Start using ngModel in variant component

--- src/app/offer-configurator/variant/variant.component.ts
+++ src/app/offer-configurator/variant/variant.component.ts
@@ -1,12 +1,13 @@
-import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Variant } from './variant.model';
+import { Feature } from './feature/feature.model';
 
 @Component({
   selector: 'app-variant',
   templateUrl: './variant.component.html',
   styleUrls: ['./variant.component.scss']
 })
-export class VariantComponent implements OnInit, OnChanges {
+export class VariantComponent implements OnInit {
 
   @Input() variant: Variant;
 
@@ -15,8 +16,6 @@ export class VariantComponent implements OnInit, OnChanges {
   @Output() selected = new EventEmitter<void>();
 
   @Output() featureBlur = new EventEmitter<void>();
-
-  values: number[][];
 
   get isDisabled(): boolean {
     return this.variant.isDisabled;
@@ -27,17 +26,15 @@ export class VariantComponent implements OnInit, OnChanges {
   ngOnInit() {
   }
 
-  ngOnChanges(changes: SimpleChanges) {
-    if (changes.variant) {
-      this.values = changes.variant.currentValue.featureGroups.map(featureGroup => featureGroup.features.map(feature => feature.value));
-    }
-  }
-
   onSelect() {
     this.selected.emit();
   }
 
   onBlur() {
     this.featureBlur.emit();
+  }
+
+  onFeatureValueChange(feature: Feature, newValue: number) {
+    console.log(`old: ${feature.value}, new: ${newValue}`);
   }
 }
